a713fe20a444dcfd4b410ab7167b177a97d89b2d
Khairee-99 <133735634+Khairee-99@users.noreply.github.com>
2023-05-29 12:37:02 +0800

    Latest

--- js.js
+++ js.js
@@ -61,3 +61,28 @@ function toggle() {
     }
 }
 
+const inputs = [
+    {
+      input: document.getElementById('name'),
+      label: document.getElementById('namelbl')
+    },
+    {
+      input: document.getElementById('email'),
+      label: document.getElementById('emaillbl')
+    },
+    {
+      input: document.getElementById('message'),
+      label: document.getElementById('msglbl')
+    }
+  ];
+  
+  inputs.forEach(({ input, label }) => {
+    input.addEventListener('input', function() {
+      if (input === document.activeElement || input.value.trim() !== '' || input.value.length !== 0) {
+        label.classList.add('moveUp');
+        label.classList.remove('typewriter');
+      } else {
+        label.classList.remove('moveUp');
+      }
+    });
+  });
