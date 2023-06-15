accfc9095e813734ccc01351c6584c572b1a70ec
Khairee-99 <133735634+Khairee-99@users.noreply.github.com>
2023-06-15 18:16:59 +0800

    This is the latest version

--- js.js
+++ js.js
@@ -1,41 +1,33 @@
-const biodatanav = document.querySelector('.nav-biodata');
+const explorebtn = document.querySelector('.explore-btn');
 const biosect = document.getElementById('biodata-section');
 
-const expnav = document.querySelector('.nav-experience');
+const experiencebtn = document.querySelector('.experiencebtn');
 const expsect = document.getElementById('experience-section');
+
+const contactbtn = document.querySelector('.contactbtn'); 
+const connectbtn = document.querySelector('.connectbtn');
+const contactsect = document.getElementById('contact-section');   
 
 const aboutnav = document.querySelector('.nav-aboutme');
 const aboutsect = document.getElementById('intro-section');
 
-const contactnav = document.querySelector('.nav-contact'); 
-const contactsect = document.getElementById('contact-section');   
-
-const abtbtn = document.querySelector('.about-btn');
-const expbtn = document.querySelector('.exp-btn');
 
 
-aboutnav.addEventListener('click', function() {
-    aboutsect.scrollIntoView({ behavior: 'smooth', block: 'center' });
-});
-
-biodatanav.addEventListener('click', function() {
+explorebtn.addEventListener('click', function() {
     biosect.scrollIntoView({ behavior: 'smooth', block: 'center' });
 });
 
-expnav.addEventListener('click', function() {
-    expsect.scrollIntoView({ behavior: 'smooth', block: 'center' });
-});
-
-contactnav.addEventListener('click', function() {
+contactbtn.addEventListener('click', function() {
     contactsect.scrollIntoView({ behavior:'smooth', block: 'center' });
 });
 
-abtbtn.addEventListener('click', function() {
-    biosect.scrollIntoView({ behavior:'smooth', block: 'center' });
+connectbtn.addEventListener('click', function() {
+  contactsect.scrollIntoView({ behavior:'smooth', block: 'center' });
 });
 
-expbtn.addEventListener('click', function() {
-    expsect.scrollIntoView({ behavior:'smooth', block: 'center' });
+
+experiencebtn.addEventListener('click', function() {
+  expsect.scrollIntoView({ behavior:'smooth', block: 'center' });  
 });
 
 const inputs = [
