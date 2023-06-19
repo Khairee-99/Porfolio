ed1d50084a4ba0ece53793117566be69d8b377b3
Khairee-99 <133735634+Khairee-99@users.noreply.github.com>
2023-06-19 18:02:59 +0800

    I think i have completed the mobile version

--- js.js
+++ js.js
@@ -45,63 +45,10 @@ const inputs = [
   }
 ];
 
-const spanAnim = document.getElementById('lblspan');
-
-inputs.forEach(({ input, label }) => {
-  input.addEventListener('input', handleInput);
-  input.addEventListener('blur', handleBlur);
-  function handleInput() {
-    if (input.value.trim() !== '') {
-      label.classList.add('moveUp');
-      spanAnim.classList.add('dissapear');
-      label.classList.remove('typewriter');
-    } else {
-      label.classList.remove('moveUp');
-      label.classList.add('typewriter');
-    }
-  }
-
-  function handleBlur() {
-    if (input.value.trim() === '') {
-      label.classList.remove('moveUp');
-    }
-  }
-});
-
-document.addEventListener('click', function(event) {
-  const clickedElement = event.target;
-  if (!isInputOrLabel(clickedElement)) {
-    inputs.forEach(({ input }) => input.blur());
-  }
-});
-
-function isInputOrLabel(element) {
-  return element.tagName === 'INPUT' || element.tagName === 'LABEL';
-}
 
 
-const radexp = document.getElementById('radio1');
-const radskill = document.getElementById('radio2');
 
-radexp.addEventListener('click', toggle);
-radskill.addEventListener('click', toggle);
 
-function toggle() {
-    let cw1 = document.querySelector('.cw-1');
-    let cw2 = document.querySelector('.cw-2');
-
-    if (radexp.checked) {
-        cw1.classList.remove('hide');
-        cw1.classList.add('active');
-        cw2.classList.remove('active');
-        cw2.classList.add('hide');
-    } else if (radskill.checked) {
-        cw1.classList.remove('active');
-        cw1.classList.add('hide');
-        cw2.classList.remove('hide');
-        cw2.classList.add('active');
-    }
-}
 
 
   
